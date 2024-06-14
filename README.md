3fa9a04aee07cfa6502972bc6393f673c9c29b39
hoangttdgwasia <hoang-tt@dgw.asia>
2024-06-14 17:01:38 +0700

    update code

--- README.md
+++
@@ -1,3 +0,0 @@
-# stackblitz-vue3-v54sud
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hoangttdgwasia/stackblitz-vue3-v54sud)
---
+++ vue-project/src/views/HelloView.vue
@@ -0,0 +1,19 @@
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="about">
+  
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
